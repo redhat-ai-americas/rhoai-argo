097a0bfc45d72c75116e14b25a46f9f29f981c36
Brian Ball <briball@umich.edu>
2026-05-01 09:24:56 -0400

    Fix the job so it doesn't require yq.

--- helm/rhoai-stack/files/enable-spark-ui.sh
+++ helm/rhoai-stack/files/enable-spark-ui.sh
@@ -2,11 +2,23 @@ oc patch deployment/spark-operator-controller --type=json -p='[{"op": "remove", 
 
 sleep 3
 
-mkdir -p scratch/
 CONSOLE_URL=$(oc get route console -n openshift-console -o go-template='{{if .spec.tls}}https://{{else}}http://{{end}}{{.spec.host}}{{"\n"}}')
 BASE_URL=.apps$(echo $CONSOLE_URL | sed 's/^.*apps//g')
 export INGRESS_URL_ARG='--ingress-url-format=https://{{$appName}}-{{$appNamespace}}'$BASE_URL
-oc get deployment -n spark-operator spark-operator-controller -o yaml > scratch/manager.yaml
-yq -i '.spec.template.spec.containers[0].args += strenv(INGRESS_URL_ARG)' scratch/manager.yaml
-yq -i '.spec.template.spec.containers[0].args += "--ingress-tls=[{}]"' scratch/manager.yaml
-oc apply -f scratch/manager.yaml
+#oc get deployment -n spark-operator spark-operator-controller -o yaml > /tmp/manager.yaml
+#yq -i '.spec.template.spec.containers[0].args += strenv(INGRESS_URL_ARG)' /tmp/manager.yaml
+#yq -i '.spec.template.spec.containers[0].args += "--ingress-tls=[{}]"' /tmp/manager.yaml
+#oc apply -f /tmp/manager.yaml
+
+oc patch deployment spark-operator-controller -n spark-operator --type='json' -p="[
+  {
+    \"op\": \"add\",
+    \"path\": \"/spec/template/spec/containers/0/args/-\",
+    \"value\": \"$INGRESS_URL_ARG\"
+  },
+  {
+    \"op\": \"add\",
+    \"path\": \"/spec/template/spec/containers/0/args/-\",
+    \"value\": \"--ingress-tls=[{}]\"
+  }
+]"
