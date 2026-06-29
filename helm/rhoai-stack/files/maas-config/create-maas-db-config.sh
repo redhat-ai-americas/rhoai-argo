779094ff2b544ce1390ecae6c6f5caa257f79598
Brian Ball <briball@umich.edu>
2026-06-29 13:31:19 -0400

    Move files around a bit.

--- helm/rhoai-stack/files/maas-config/create-maas-db-config.sh
+++ helm/rhoai-stack/files/maas-config/create-maas-db-config.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 uri=$(oc get secret openshift-ai-maas-app -ojsonpath='{.data.uri}' | base64 -d)
 oc create secret generic maas-db-config -n redhat-ods-applications --from-literal=DB_CONNECTION_URL="$uri" --dry-run=client -oyaml | oc apply -f-
