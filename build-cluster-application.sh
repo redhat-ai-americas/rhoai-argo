f3e012cdf4db92254328e918437007879e573506
David Clark <davclark@redhat.com>
2026-07-15 16:46:11 -0500

    Features selected from maas-aro fork

--- build-cluster-application.sh
+++ build-cluster-application.sh
@@ -14,5 +14,12 @@ NEWFILE=cluster-applications/${BASE_URL:6}-$(basename $1)
 cp $1 $NEWFILE
 echo "New file location is $NEWFILE"
 
+SERVICE_CA_FILE=$(mktemp)
+oc get configmap signing-cabundle -n openshift-service-ca \
+  -o jsonpath='{.data.ca-bundle\.crt}' > "$SERVICE_CA_FILE"
+
 yq -i ".spec.source.helm.valuesObject.global.clusterBaseUrl = \"$BASE_URL\"" $NEWFILE
+yq -i ".spec.source.helm.valuesObject.global.serviceCABundle = load_str(\"$SERVICE_CA_FILE\")" $NEWFILE
+rm -f "$SERVICE_CA_FILE"
+
 oc apply -f $NEWFILE
